cc5779fc5177d91201464cb163712d465935113f
Fabio-cristovao <fabio.ascristovao.com>
2023-06-02 15:12:53 +0100

    dynamic user input generating the grid, cells being added bg prop

--- index.js
+++ index.js
@@ -1,40 +1,57 @@
 // build a grid of 16 x 16 divs
 
 // create a container div
-const container = document.createElement('div');
-const gridContainer = document.querySelector(".site-container");
-container.classList.add('container');
-gridContainer.appendChild(container);
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
+const siteContainer = document.querySelector('.site-container')
+let gridContainer = document.createElement('div');
+siteContainer.appendChild(gridContainer);
+const input = document.querySelector(".input");
+const submitButton = document.querySelector(".submit-button");
+console.log(submitButton);
+console.log(input);
+
+// identify the submit button to make call the function with the desired number of cells
+
+function makeRows(e) {
+  e.preventDefault();
+  gridContainer.classList.add('game-container');
+  siteContainer.appendChild(gridContainer);
+  console.log(gridContainer);
+  console.log("button clicked!");
+  console.log(e.target);
+  const numberOfCells = input.value;
+  gridContainer.style.setProperty('--grid-rows', numberOfCells);
+  gridContainer.style.setProperty('--grid-cols', numberOfCells);
+
+  gridContainer.innerHTML = "";
+
+  for (c = 0; c < (numberOfCells * numberOfCells); c++) {
     let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
+    cell.classList.add("grid-item");
+    gridContainer.appendChild(cell);
+    cell.classList.add("grid-item");
+
+    console.log(cell);
   };
+  const gridItems = document.querySelectorAll('.grid-item');
+  gridItems.forEach(gridItem => gridItem.addEventListener('mouseenter', changeColor, false));
+
+
+
+
+
+
 }
 
-makeRows(100, 100);
+submitButton.addEventListener('click', makeRows, false);
 
 // add an event listener for each time mouse enters and leaves a grid item
-const gridItems = document.querySelectorAll('.grid-item');
-console.log(gridItems);
-gridItems.forEach(gridItem => gridItem.addEventListener('mouseenter', changeColor));
-gridItems.forEach(gridItem => gridItem.addEventListener('mouseleave', changeColor));
+
 
 // change the background color of the grid item
 function changeColor(e) {
+  console.log("cell entered");
   e.target.style.backgroundColor = 'black';
-}
-
-// create a grid of divs
-function createGrid(size) {
-  for (let i = 0; i < size * size; i++) {
-    const div = document.createElement('div');
-    div.classList.add('grid');
-    container.appendChild(div);
-  }
 }
 
 // create a grid of 16 x 16 divs
