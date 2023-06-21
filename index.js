9ba3257f900f6513355092b79f6c385810fc68d4
Fabio-cristovao <fabio.ascristovao.com>
2023-06-21 17:53:42 +0100

    rainbow painting funcionatily added

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 
 const siteContainer = document.querySelector('.container')
 const submitButton = document.querySelector(".submit-button");
+let randomColorBtn = document.querySelector(".random");
 let clearBtn = document.querySelector('.clear');
 let gridContainer = document.createElement('div');
 siteContainer.appendChild(gridContainer);
 const input = document.querySelector(".input");
 console.log(submitButton);
 console.log(input);
+
+submitButton.addEventListener('click', makeRows, false);
+
+randomColorBtn.addEventListener('click', ChangeRandomColor, false)
+clearBtn.addEventListener('click', clearGameContainer, false);
 
 function makeRows(e) {
   e.preventDefault();
@@ -26,19 +32,23 @@ function makeRows(e) {
     cell.classList.add("grid-item");
     gridContainer.appendChild(cell);
     cell.classList.add("grid-item");
-
     console.log(cell);
   };
-  const gridItems = document.querySelectorAll('.grid-item');
-  gridItems.forEach(gridItem => gridItem.addEventListener('mouseenter', changeColor, false));
 }
 
-submitButton.addEventListener('click', makeRows, false);
-clearBtn.addEventListener('click', clearGameContainer, false);
-
-function changeColor(e) {
+function changeColorBlack(e) {
   e.target.style.backgroundColor = 'black';
 }
+
+function ChangeRandomColor(e) {
+  console.log('rainbow button clicked');
+  let gridItems = document.querySelectorAll('.grid-item');
+  gridItems.forEach(item => {
+    item.addEventListener('mouseenter', changeRandomColor, false)
+    console.log(item);
+  })
+  console.log();
+};
 
 function clearColor(e) {
   e.target.style.backgroundColor = 'white';
@@ -47,8 +57,39 @@ function clearColor(e) {
 function clearGameContainer(e) {
   console.log(clearBtn);
   makeRows(e);
-
 };
+
+function changeRandomColor() {
+  console.log('function executed!');
+  let gridItems = document.querySelectorAll('.grid-item');
+  gridItems.forEach(item => {
+    item.addEventListener('mouseenter', getRandomColor, false)
+  })
+}
+
+function getRandomColor(e) {
+  console.log(e.target);
+  e.target.style.backgroundColor = chooseRandomColor();
+}
+
+function chooseRandomColor() {
+  let colors = [
+    '#D84B20',
+    '#FFD65E',
+    '#7BC8A4',
+    '#4C6EAD',
+    '#735CDD',
+    '#C281B5',
+  ];
+
+  let randomColor = Math.floor(Math.random() * colors.length);
+  console.log(randomColor);
+  return colors[randomColor];
+}
+
+
+
+
 
 
 
